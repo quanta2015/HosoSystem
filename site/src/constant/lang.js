5c796e4a8c85d8b32f82bc4ec25eb107ebabf600
kirakira448 <603183744@qq.com>
2024-03-11 17:11:04 +0900

    日本語訳

--- site/src/constant/lang.js
+++ site/src/constant/lang.js
@@ -1,18 +1,18 @@
 
 export const JP = {
   NAV: {
-    MENU_L1:'基本信息管理',
-    MENU_L1_1:'營業所管理',
-    MENU_L1_2:'現場管理',
-    MENU_L1_3:'用戶管理',
-    MENU_L1_4:'製品管理',
-    MENU_L1_5:'種類設定',
-    MENU_L1_6:'サプライヤー管理',
-    MENU_L1_7:'倉庫管理',
-    MENU_L2:'部品管理',
-    MENU_L2_1:'在庫管理',
-    MENU_L2_2:'出入庫管理',
-    MENU_L2_3:'出入庫審核',
+    MENU_L1: '基本信息管理',
+    MENU_L1_1: '營業所管理',
+    MENU_L1_2: '現場管理',
+    MENU_L1_3: '用戶管理',
+    MENU_L1_4: '製品管理',
+    MENU_L1_5: '種類設定',
+    MENU_L1_6: 'サプライヤー管理',
+    MENU_L1_7: '倉庫管理',
+    MENU_L2: '部品管理',
+    MENU_L2_1: '在庫管理',
+    MENU_L2_2: '出入庫管理',
+    MENU_L2_3: '出入庫審核',
   },
 
   TXT: {
@@ -21,10 +21,10 @@ export const JP = {
     DEP_NAME: '營業所名稱',
     DEP_ADDR: "營業所地址",
     DEP_PHONE: "營業所電話",
-    SUP_IMG:'アイコン',
-    SUP_NAME:'サプライヤー名',
-    SUP_PHONE:'連絡先',
-    SUP_ADDR:'サプライヤーアドレース',
+    SUP_IMG: 'アイコン',
+    SUP_NAME: 'サプライヤー名',
+    SUP_PHONE: '連絡先',
+    SUP_ADDR: 'サプライヤーアドレース',
   },
 
   FN: {
@@ -37,7 +37,7 @@ export const JP = {
     NO: 'いいえ',
     CLS: 'キャンセル',
     SAV: 'セーブ',
-    
+
   },
 
   MSG: {
@@ -48,82 +48,81 @@ export const JP = {
   },
 
   DB: {
-    PART:{
-      QRCODE:'QRコード',
-      IMG:'製品画像',
-      CODE:'製品ID',
-      NAME:'製品名前',
-      MOD_NAME:'製品種類',
-      SUP_NAME:'サプライヤー',
-      SUP_PHONE:'サプライヤー電話番号',
-      SUP_ADDR:'サプライヤーアドレス',
+    PART: {
+      QRCODE: 'QRコード',
+      IMG: '製品画像',
+      CODE: '製品ID',
+      NAME: '製品名前',
+      MOD_NAME: '製品種類',
+      SUP_NAME: 'サプライヤー',
+      SUP_PHONE: 'サプライヤー電話番号',
+      SUP_ADDR: 'サプライヤーアドレス',
     },
-    MODEL:{
-      ID:'サプライヤーアドレス',
-      NAME:'類型',
+    MODEL: {
+      ID: 'サプライヤーアドレス',
+      NAME: '類型',
     },
-    SUPPLY:{
-      ID:'番号',
-      IMG:'アイコン',
-      NAME:'サプライヤー名',
-      PHONE:'連絡先',
-      ADDR:'サプライヤーアドレス',
+    SUPPLY: {
+      ID: '番号',
+      IMG: 'アイコン',
+      NAME: 'サプライヤー名',
+      PHONE: '連絡先',
+      ADDR: 'サプライヤーアドレス',
     },
-    WARE:{
-      ID:'番号',
-      DEP_NAME:'所屬營業所',
-      CODE:'倉庫編碼',      
-      NAME:'倉庫名稱',
-      ADDR:'倉庫地址',
-      MANAGER:'負責人',
+    WARE: {
+      ID: '番号',
+      DEP_NAME: '所属営業所',
+      CODE: '倉庫コード',
+      NAME: '倉庫名',
+      ADDR: '倉庫住所',
+      MANAGER: '責任者'
     },
-    STOCK:{
-      ID:'番号',
-      WARE_CODE:'倉庫編碼',
-      WARE_NAME:'倉庫名稱',      
-      PART_IMG:'部品圖標',
-      PART_CODE:'部品編碼',
-      PART_NAME:'部品名稱',
-      MODEL_NAME:'部品類型',
-      SUPPLY_NAME:'供應商',
-      NUM:'數量',
-      STATUS:'狀態',
+    STOCK: {
+      ID: '番号',
+      WARE_CODE: '倉庫コード',
+      WARE_NAME: '倉庫名',
+      PART_IMG: '部品画像',
+      PART_CODE: '部品コード',
+      PART_NAME: '部品名',
+      MODEL_NAME: '部品タイプ',
+      SUPPLY_NAME: 'サプライヤー',
+      NUM: '数量',
+      STATUS: '状態'
     },
-    STOCK_IO:{
-      ID:'番号',
-      RECEPT_CODE:'出入庫單號',
-      WARE_NAME:'倉庫名稱',      
-      PART_IMG:'部品圖標',
-      PART_CODE:'部品編碼',
-      PART_NAME:'部品名稱',
-      MODEL_NAME:'部品類型',
-      SUPPLY_NAME:'供應商',
-      NUM:'數量',      
+    STOCK_IO: {
+      ID: '番号',
+      RECEPT_CODE: '入出庫番号',
+      WARE_NAME: '倉庫名',
+      PART_IMG: '部品画像',
+      PART_CODE: '部品コード',
+      PART_NAME: '部品名',
+      MODEL_NAME: '部品タイプ',
+      SUPPLY_NAME: 'サプライヤー',
+      NUM: '数量'
     },
-    USER:{
-      ID:'編號',
-      DEP_NAME:'所屬部門',
-      USR:'賬號',
-      NAME:'用戶名',
-      PWD:'密碼',
-      ROLE:'角色'
+    USER: {
+      ID: '番号',
+      DEP_NAME: '所属部門',
+      USR: 'アカウント',
+      NAME: 'ユーザー名',
+      PWD: 'パスワード',
+      ROLE: '役割'
     },
     DEP: {
-      ID: '編號',
-      DEP_NAME: '營業所',
-      DEP_ADDR: '地址',
-      DEP_PHONE:'聯繫方式'
+      ID: '番号',
+      DEP_NAME: '営業所',
+      DEP_ADDR: 'アドレース',
+      DEP_PHONE: '連絡先'
     },
-    SITE:{
-      ID: '編號',
-      DEP_NAME: '所屬營業所',
-      NAME: '現場名稱',
-      ADDR: '地址',
-      PHONE: '聯繫方式',
-      MANAGER: '負責人',
+    SITE: {
+      ID: '番号',
+      DEP_NAME: '所属営業所',
+      NAME: '現場名',
+      ADDR: 'アドレース',
+      PHONE: '連絡先',
+      MANAGER: '責任者'
     }
   }
 
-  
-  
+
 }
